a00438633d095b204619bd09fe352cc2fe226ee2
ade-m <66313509+ade-m@users.noreply.github.com>
2022-05-31 19:36:48 +0700

    Luas Lingkaran

--- src/App.java
+++ src/App.java
@@ -3,12 +3,16 @@ public class App {
         String pesan = "Selamat Datang di matakuliah Pemrograman Berorientasi Objek";
         Integer tahun = 2022;
         int tahun2 = 2022;
-        
+        String penutup= "Sampai Jumpa";
         System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
         System.out.println(pesan + " Tahun " + tahun);
         System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
         
         hitungLuasKubus();
+        Lingkaran lingkaran = new Lingkaran();
+        lingkaran.r=15;
+        //lingkaran.PI=40;
+        lingkaran.tampilkanLuasLingkaran(); 
     }
     //hitung volume kubus => V=s3
     public static void hitungLuasKubus(){
@@ -19,4 +23,5 @@ public class App {
         System.out.println("Panjang Sisi = " +s + " cm");
         System.out.println("Volume Kubus = s*s*s = " +v + " cm3");
     }
+ 
 }
